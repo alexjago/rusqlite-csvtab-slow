6c09c2f59594b487884b0f16cb2062df11375091
Alex <4475543+alexjago@users.noreply.github.com>
2020-11-30 07:50:37 +1000

    The solution is to load the tables into memory first...

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use rusqlite::{params, Connection, Result, NO_PARAMS};
+use std::time::{Instant};
 
 fn main() {
-    println!("This crate should hopefully demonstrate a performance issue in Rusqlite's csvtab module...");
+    println!("This crate should hopefully demonstrate a performance issue in Rusqlite's csvtab module: multi-table queries must have the tables in memory for performance.");
     println!("Make sure the two files from `csvs.zip`, `stop_times.txt` and `trips.txt`, are in the working directory...");
-    let db = Connection::open_in_memory().expect("Could not open virtual database");
+    println!("First we're going to do three queries with virtual tables (i.e. on-disk).");
+    println!("Then we're going to load the tables into memory properly and repeat.");
+    let db = Connection::open_in_memory().expect("Could not open in-memory database");
 
     rusqlite::vtab::csvtab::load_module(&db)
         .expect("Could not load CSV module of virtual database");
@@ -11,50 +14,63 @@ fn main() {
     load_gtfs(&db).expect("Failed loading GTFS (are the files there?)");
 
     println!("This first query should complete quickly");
-    query_one(&db);
+    query_one(&db, 'v');
 
-    println!("\nThis second query be slow in Rust, but was almost as quick as #1 in the SQLite shell.");
-    query_slow(&db, 893);
+    println!("This second query be slow in Rust, but was almost as quick as #1 in the SQLite shell. This is because SQLite loads the file into memory, but we haven't yet. Expect this query to take several minutes...");
+    query_slow(&db, 893, 'v');
     
-    println!("\nThis third query is like #2 but should be quick, with many fewer rows involved.");
-    query_slow(&db, 313178);
+    println!("This third query is like #2 but should be quick, with many fewer rows involved.");
+    query_slow(&db, 313178, 'v');
     
+    println!("\nNow we'll load the tables into memory and create another view...");
+    load_memory(&db).expect("Failed loading into memory");
+    
+    println!("Query #1 should still be fast");
+    query_one(&db, 'm');
+    
+    println!("Query #2 should now also be fast");
+    query_slow(&db, 893, 'm');
+
+    println!("Query #3 should still be fast");
+    query_slow(&db, 313178, 'm');    
 }
 
 fn load_gtfs(db: &Connection) -> Result<(), rusqlite::Error> {
     //! Loads all the GTFS CSVs into SQLite tables in `db`
 
-    for (t, p) in [("StopTimes", "stop_times.txt"), ("Trips", "trips.txt")].iter() {
+    for (t, p) in [("StopTimesV", "stop_times.txt"), ("TripsV", "trips.txt")].iter() {
         let schema = format!(
             "CREATE VIRTUAL TABLE {} USING csv(filename='{}', header=YES)",
             &t, &p
         );
 
-        eprintln!("{}\n", schema);
+        println!("{}\n", schema);
 
         db.execute_batch(&schema)?;
     }
 
-    let q = "CREATE VIEW TripSeqs AS
-SELECT StopTimes.stop_id, StopTimes.stop_sequence, Trips.direction_id, Trips.route_id
-FROM StopTimes
-INNER JOIN Trips on StopTimes.trip_id = Trips.trip_id;";
+    let q = "CREATE VIEW TripSeqsV AS
+SELECT StopTimesV.stop_id, StopTimesV.stop_sequence, TripsV.direction_id, TripsV.route_id
+FROM StopTimesV
+INNER JOIN TripsV on StopTimesV.trip_id = TripsV.trip_id;";
     
-    eprintln!("{}\n", q);
+    println!("{}\n", q);
     
     db.execute_batch(q)?;
 
     Ok(())
 }
 
-fn query_one(db: &Connection) {
+fn query_one(db: &Connection, v_or_m: char) {
     //! Execute the first, fast query on a single table
     
-    let q = "SELECT count(*) FROM StopTimes WHERE stop_id IS '893';";
-    eprintln!("{}", q);
+    let q = format!("SELECT count(*) FROM StopTimes{} WHERE stop_id IS '893';", v_or_m);
+    println!("{}", q);
+    
+    let now = Instant::now();
     
     let mut stmt = db
-        .prepare(q)
+        .prepare(&q)
         .unwrap();
     let rows = stmt
         .query_map(NO_PARAMS, |r| r.get(0))
@@ -64,13 +80,17 @@ fn query_one(db: &Connection) {
         let t: i64 = r.unwrap();
         println!("{:?}", t);
     }
+    
+    println!("Completed in {} seconds.\n", now.elapsed().as_secs());
 }
 
-fn query_slow(db: &Connection, stop_id: usize) {
+fn query_slow(db: &Connection, stop_id: usize, v_or_m: char) {
     //! Execute the INNER JOIN query
 
-    let q = format!("SELECT count(*) FROM TripSeqs WHERE stop_id IS '{}';", stop_id);
-    eprintln!("{}", q);
+    let q = format!("SELECT count(*) FROM TripSeqs{} WHERE stop_id IS '{}';", v_or_m, stop_id);
+    println!("{}", q);
+
+    let now = Instant::now();
 
     let mut stmt = db
         .prepare(&q)
@@ -84,6 +104,36 @@ fn query_slow(db: &Connection, stop_id: usize) {
     let r: i64 = rows.next().unwrap().unwrap().get_unwrap(0);
 
     println!("{:?}", r);
+    
+    println!("Completed in {} seconds.\n", now.elapsed().as_secs());
+
 }  
 
+fn load_memory(db: &Connection) -> Result<(), rusqlite::Error> {
+    println!("Loading the tables into memory...");
+    
+    for t in ["StopTimes", "Trips"].iter() {
+        let schema = format!(
+            "CREATE TABLE {}M AS SELECT * FROM {}V;",
+            &t, &t
+        );
+
+        println!("{}\n", schema);
+
+        let now = Instant::now();
+        db.execute_batch(&schema)?;
+        println!("Loaded {} in {} seconds.\n", &t, now.elapsed().as_secs());
+    }
+
+    let q = "CREATE VIEW TripSeqsM AS
+SELECT StopTimesM.stop_id, StopTimesM.stop_sequence, TripsM.direction_id, TripsM.route_id
+FROM StopTimesM
+INNER JOIN TripsM on StopTimesM.trip_id = TripsM.trip_id;";
+    
+    println!("{}\n", q);
+    
+    db.execute_batch(q)?;
+
+    Ok(())
+}
 
